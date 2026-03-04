35c2421c8a93253072c26a5d1b8c96ebb7e3980d
Cristhian Ortega <cristhian.ortega@ibtmx.com>
2026-03-04 00:30:55 -0600

    Teoría y práctica de Factory Function

--- 01-creacionales/07-factory-function.ts
+++ 01-creacionales/07-factory-function.ts
@@ -7,3 +7,29 @@
  * * es decir, en tiempo de ejecución y no en tiempo de compilación.
  *
  */
+
+type Language = "es" | "en" | "fr";
+
+function createGreeter(language: Language) {
+  return function (name: string) {
+    const messages = {
+      es: `Hola, ${name}`,
+      en: `Hello, ${name}`,
+      fr: `Bonjour, ${name}`,
+    };
+
+    return console.log(messages[language]);
+  };
+}
+
+function main() {
+  const spanishGreeter = createGreeter("es");
+  const englishGreeter = createGreeter("en");
+  const frenchGreeter = createGreeter("fr");
+
+  spanishGreeter("Fernando");
+  englishGreeter("Alice");
+  frenchGreeter("Pierre");
+}
+
+main();
