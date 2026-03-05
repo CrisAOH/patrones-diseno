3916fab34606ac7afa76a653f59dd7c76242c6e0
Cristhian Ortega <cristhian.ortega@ibtmx.com>
2026-03-05 01:42:35 -0600

    Teoría y práctica de Decorator

--- 02-estructurales/04-decorator.ts
+++ 02-estructurales/04-decorator.ts
@@ -11,3 +11,57 @@
  *
  * https://refactoring.guru/es/design-patterns/decorator
  */
+
+interface Notification {
+  send(message: string): void;
+}
+
+class BasicNotification implements Notification {
+  send(message: string): void {
+    console.log(`Enviando notificación básica: ${message}`);
+  }
+}
+
+abstract class NotificationDecorator implements Notification {
+  protected notification: Notification;
+
+  constructor(notification: Notification) {
+    this.notification = notification;
+  }
+
+  send(message: string): void {
+    this.notification.send(message);
+  }
+}
+
+class EmailDecorator extends NotificationDecorator {
+  private sendEmail(message: string) {
+    console.log(`Enviando notificación por correo electrónico: ${message}`);
+  }
+
+  override send(message: string): void {
+    super.send(message);
+    this.sendEmail(message);
+  }
+}
+
+class SmsDecorator extends NotificationDecorator {
+  private sendSms(message: string) {
+    console.log(`Enviando notificación por correo electrónico: ${message}`);
+  }
+
+  override send(message: string): void {
+    super.send(message);
+    this.sendSms(message);
+  }
+}
+
+function main() {
+  let notification: Notification = new BasicNotification();
+  notification = new EmailDecorator(notification);
+  notification = new SmsDecorator(notification);
+
+  notification.send("Alerta de sistema!");
+}
+
+main();
