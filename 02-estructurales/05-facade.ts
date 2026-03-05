dac4fa1c5cccd0cc8b8e526002565991841d86f9
Cristhian Ortega <cristhian.ortega@ibtmx.com>
2026-03-05 02:11:55 -0600

    Teoría y práctica de Facade.

--- 02-estructurales/05-facade.ts
+++ 02-estructurales/05-facade.ts
@@ -11,3 +11,122 @@
  *
  * https://refactoring.guru/es/design-patterns/facade
  */
+
+import { off } from "node:process";
+
+class Projector {
+  on() {
+    console.log("Proyector encendido.");
+  }
+
+  turnOff() {
+    console.log("Proyector apagado.");
+  }
+}
+
+class SoundSystem {
+  on() {
+    console.log("Sistema de sonido encendido.");
+  }
+
+  off() {
+    console.log("Sistema de sonido apagado.");
+  }
+}
+
+class VideoPlayer {
+  on() {
+    console.log("Video player encendido.");
+  }
+
+  play(movie: string) {
+    console.log(`Reproduciendo ${movie}`);
+  }
+
+  stop() {
+    console.log("Película detenida");
+  }
+
+  off() {
+    console.log("Video player apagado");
+  }
+}
+
+class PopcornMaker {
+  makingPopcorn() {
+    console.log("Haciendo palomitas");
+  }
+
+  stopMakingPopcorn() {
+    console.log("Apagándo máquina.");
+  }
+}
+
+interface HomeTheatreFacadeOptions {
+  projector: Projector;
+
+  soundSystem: SoundSystem;
+
+  videoPlayer: VideoPlayer;
+
+  popcornMaker: PopcornMaker;
+}
+
+class HomeTheatreFacade {
+  private projector: Projector;
+  private soundSystem: SoundSystem;
+  private videoPlayer: VideoPlayer;
+  private popcornMaker: PopcornMaker;
+
+  constructor({
+    projector,
+    soundSystem,
+    videoPlayer,
+    popcornMaker,
+  }: HomeTheatreFacadeOptions) {
+    this.projector = projector;
+    this.soundSystem = soundSystem;
+    this.videoPlayer = videoPlayer;
+    this.popcornMaker = popcornMaker;
+  }
+
+  watchMovie(movie: string): void {
+    console.log(`Preparando para ver la película.`);
+    this.projector.on();
+    this.soundSystem.on();
+    this.popcornMaker.makingPopcorn();
+    this.videoPlayer.on();
+    this.videoPlayer.play(movie);
+
+    console.log("Disfrute la película.");
+  }
+
+  endWatchingMovie(): void {
+    console.log(`Preparando para detener la película.`);
+    this.projector.turnOff();
+    this.soundSystem.off();
+    this.popcornMaker.stopMakingPopcorn();
+    this.videoPlayer.stop();
+    this.videoPlayer.off();
+
+    console.log("Sistema apagado.");
+  }
+}
+
+function main() {
+  const projector = new Projector();
+  const popcornMaker = new PopcornMaker();
+  const soundSystem = new SoundSystem();
+  const videoPlayer = new VideoPlayer();
+
+  const homeTheatre: HomeTheatreFacade = new HomeTheatreFacade({
+    projector,
+    popcornMaker,
+    soundSystem,
+    videoPlayer,
+  });
+
+  homeTheatre.watchMovie("Coraline");
+}
+
+main();
