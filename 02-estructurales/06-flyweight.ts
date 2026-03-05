2cf4b7966d2bab754ca50bf8bb4e480008e556a8
Cristhian Ortega <cristhian.ortega@ibtmx.com>
2026-03-05 02:48:44 -0600

    Teoría y práctica de Flyweight.

--- 02-estructurales/06-flyweight.ts
+++ 02-estructurales/06-flyweight.ts
@@ -8,3 +8,64 @@
  *
  * https://refactoring.guru/es/design-patterns/flyweight
  */
+
+interface Location {
+  display(coordinates: { x: number; y: number }): void;
+}
+
+class LocationIcon implements Location {
+  private type: string;
+  private iconImage: string;
+
+  constructor(type: string, iconImage: string) {
+    this.type = type;
+    this.iconImage = iconImage;
+  }
+
+  display(coordinates: { x: number; y: number }): void {
+    console.log(
+      `Coords: ${this.type} en ${coordinates.x}, ${coordinates.y} con icono ${this.iconImage}`,
+    );
+  }
+}
+
+class LocationFactory {
+  private icons: Record<string, LocationIcon> = {};
+
+  getLocationIcon(type: string): LocationIcon {
+    if (!this.icons[type]) {
+      console.log(`Creando nueva instancia de ${type}`);
+      const iconImage = `imagen_${type.toLowerCase()}.png`;
+      this.icons[type] = new LocationIcon(type, iconImage);
+    }
+
+    return this.icons[type];
+  }
+}
+
+class MapLocation {
+  private coordinates: { x: number; y: number };
+  private icon: LocationIcon;
+
+  constructor(x: number, y: number, icon: LocationIcon) {
+    this.coordinates = { x, y };
+    this.icon = icon;
+  }
+
+  display() {
+    this.icon.display(this.coordinates);
+  }
+}
+
+function main() {
+  const factory = new LocationFactory();
+
+  const locations = [
+    new MapLocation(10, 20, factory.getLocationIcon("hospital")),
+    new MapLocation(1, 2, factory.getLocationIcon("hospital")),
+  ];
+
+  locations.forEach((location) => location.display());
+}
+
+main();
