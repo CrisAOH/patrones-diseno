fe0200a8b8f59ec7a313f728f14c725c399376c9
Cristhian Ortega <cristhian.ortega@ibtmx.com>
2026-03-04 19:42:51 -0600

    Teoría y práctica de Bridge.

--- 02-estructurales/02-bridge.ts
+++ 02-estructurales/02-bridge.ts
@@ -9,3 +9,66 @@
  *
  * https://refactoring.guru/es/design-patterns/bridge
  */
+
+interface Ability {
+  use(): void;
+}
+
+class SwordAttack implements Ability {
+  use(): void {
+    console.log("Ataca con una espada.");
+  }
+}
+
+class MagicSpell implements Ability {
+  use(): void {
+    console.log("Lanza un hechizo.");
+  }
+}
+
+class AxeAttack implements Ability {
+  use(): void {
+    console.log("Ataca con un hacha.");
+  }
+}
+
+abstract class Character {
+  protected ability: Ability;
+
+  constructor(ability: Ability) {
+    this.ability = ability;
+  }
+
+  setAbility(ability: Ability): void {
+    this.ability = ability;
+  }
+
+  abstract performAbility(): void;
+}
+
+class Warrior extends Character {
+  override performAbility(): void {
+    console.log("El guerrero está listo para luchar.");
+    this.ability.use();
+  }
+}
+
+class Mage extends Character {
+  override performAbility(): void {
+    console.log("El mago prepara su magia.");
+    this.ability.use();
+  }
+}
+
+function main() {
+  const warrior = new Warrior(new SwordAttack());
+  warrior.performAbility();
+
+  warrior.setAbility(new AxeAttack());
+  warrior.performAbility();
+
+  const mage = new Mage(new MagicSpell());
+  mage.performAbility();
+}
+
+main();
